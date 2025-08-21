ce05d105970fdc1a35e479fff47b3c24cf22181e
Qiong Zhou Huang <qiong@phonic.co>
2025-08-21 01:20:07 +0000

    Lint it more nicely

--- src/phonic/types/config_payload.py
+++ src/phonic/types/config_payload.py
@@ -124,7 +124,9 @@ class ConfigPayload(UniversalBaseModel):
     """
 
     if IS_PYDANTIC_V2:
-        model_config: typing.ClassVar[pydantic.ConfigDict] = pydantic.ConfigDict(extra="allow", frozen=True)  # type: ignore # Pydantic v2
+        model_config: typing.ClassVar[pydantic.ConfigDict] = pydantic.ConfigDict(
+            extra="allow", frozen=True
+        )  # type: ignore # Pydantic v2
     else:
 
         class Config:
